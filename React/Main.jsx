d8961e4eb01285348568e5a2a5a056fd97a7d0ca
Unknown <jesus_ricardo1995@hotmail.com>
2018-06-28 14:16:07 -0500

    Merge branch 'release/finishConfig'
    update requeriments

--- React/Main.jsx
+++ React/Main.jsx
@@ -2,9 +2,15 @@ import React from 'react';
 
 function Main() {
     return (
-        <h1>Mai</h1>
+        <h1>Ma</h1>
     )
 };
 
 export default Main;
 
+/* Flask
+itsdangerous
+Jinja2
+MarkupSafe
+Werkzeug
+wheel*/
